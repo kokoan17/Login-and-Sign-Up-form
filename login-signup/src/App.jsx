317b19a545a85cfbb80cc206f227a057022b76e1
Konstantin <kostya1703tsoy@gmail.com>
2023-11-15 18:39:50 +0300

    first commit

---
+++ login-signup/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import LoginSignUp from './Components/LoginSignUp/LoginSignUp'
+
+function App() {
+
+  return (
+    <>
+      <LoginSignUp/>
+    </>
+  )
+}
+
+export default App
